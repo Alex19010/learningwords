f50743fcb51252a75f25cda39972e03e9f985b64
Alex19010 <sacha2005d@gmail.com>
2024-05-29 00:00:11 +0900

    create function for delete sections and words

--- words/urls.py
+++ words/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create_section/', views.create_section, name='create_section'),
     path('section/<int:section_id>/', views.show_words, name='show_words'),
     path('section/<int:section_id>/create_words/', views.create_words, name='create_words'),
+    path('section/<int:section_id>/delete_word/<int:word_id>/', views.delete_word, name='delete_word'),
+    path('delete_section/<int:section_id>/', views.delete_section, name='delete_section'),
 ]
--- words/views.py
+++ words/views.py
@@ -49,8 +49,23 @@ def create_words(request, section_id):
             new_word = Word.objects.create(foreign_word=foreign_word, native_word=native_word, section=section)
             new_word.save()
             return redirect(request.META.get('HTTP_REFERER', '/'))
-            #return render(request, 'words/words_show_words.html', context)
     return render(request, 'words/words_new_words.html', context)
+
+
+def delete_word(request, section_id, word_id):
+    word = get_object_or_404(Word, id=word_id, section=section_id)
+    word.delete()
+    context = {
+        'section_id': section_id
+    }
+    return redirect(request.META.get('HTTP_REFERER', '/'), context)
+    # return redirect('show_words', context)
+
+
+def delete_section(request, section_id):
+    section = get_object_or_404(Section, id=section_id)
+    section.delete()
+    return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
 def learn_word(request):
